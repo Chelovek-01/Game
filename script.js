dc65ce977210361ebe3618329aae5533970a31f7
Murad <chelovek477386@gmail.com>
2024-05-12 22:29:50 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function () {
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;     
+
+    const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
+
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.draw(ctx);
+        game.update(deltaTime);
+        requestAnimationFrame(animate)
+    }
+
+    animate(0);
+})
